f7e70f9b228aef7c01892645ae965ae90e069bca
daniel-budai <danielbudai@tutanota.com>
2024-06-20 19:55:46 +0200

    restaurant search, restaurant route

---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { param } from "express-validator";
+import Restaurant from "../models/restaurant";
+import RestaurantController from "../controllers/RestaurantController";
+
+const router = express.Router();
+
+router.get(
+  "/search/:city",
+  param("city").isString().trim().notEmpty().withMessage("City is required"),
+  RestaurantController.searchRestaurants
+);
+
+export default router;
