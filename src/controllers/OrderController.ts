5415988a120f1bde4db7cae1d13bbcb81a2531dc
daniel-budai <danielbudai@tutanota.com>
2024-06-26 19:46:13 +0200

    stripe webhook, CLI

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -21,6 +21,13 @@ type CheckoutSessionRequest = {
   restaurantId: string;
 };
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+  console.log("RECIEVED EVENT");
+  console.log("=====================================");
+  console.log("event: ", req.body);
+  res.send();
+};
+
 const createCheckoutSession = async (req: Request, res: Response) => {
   try {
     const checkoutSessionRequest: CheckoutSessionRequest = req.body;
@@ -127,4 +134,4 @@ const createSession = async (
   return sessionData;
 };
 
-export default { createCheckoutSession };
+export default { createCheckoutSession, stripeWebhookHandler };
